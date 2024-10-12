2f36c997fd8ce9dc40f594c5500a9c03627daece
Nneupane133 <nzn0046@auburn.edu>
2024-10-12 03:28:00 -0500

    Add files via upload

---
+++ Heyday_Nabin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Hey Day is the Auburn tradition that is hosted to recognize the braves of the soldiers who came back after the WWII. On this day, everyone is encouraged to come forward and respect the braves who saved this country. Each year, we carry on this tradition by handing out name tags and encouraging students, faculty, and the rest of the Auburn Family to greet one another, bringing all members of Auburn closer together. Hey Day has become one of Auburn University’s longest, most cherished traditions and is a day the student body has grown to love and look forward to each year.
+echo $"Hey Nabin!"
+
+
